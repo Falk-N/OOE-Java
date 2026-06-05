014c8b5facd02491a91fc9a9205ba72ac9cc9494
Lukas Fischer <lukas.fischer@yfox.eu>
2026-06-05 13:02:15 +0200

    list entry class

---
+++ src/todoapp/ListEntry.java
@@ -0,0 +1,42 @@
+package todoapp;
+
+public class ListEntry {
+    //Attributes
+    private int id;
+    private String title;
+    private boolean completed;
+
+
+    //Constructor
+    public void ListEntry (int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.completed = false;
+    }
+
+
+    //Set
+
+    public void setTitle (string title) {
+        this.title = title;
+    }
+
+    public void setCompleted (boolean completed) {
+        this.completed = completed;
+    }
+
+
+    //Get
+
+    public int getId () {
+        return this.id;
+    }
+
+    public String getTitle () {
+        return this.title;
+    }
+
+    public boolean getCompleted () {
+        return this.completed;
+    }
+}
