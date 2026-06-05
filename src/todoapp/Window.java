c51757d5070ac799d8f0e1d5cf617a26200c60ea
Falk-N <falk.natkowski@web.de>
2026-06-05 12:58:05 +0200

    Created Window

--- src/todoapp/Window.java
+++ src/todoapp/Window.java
@@ -1,12 +1,19 @@
 package todoapp;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 class Window extends JFrame {
 
     Window() {
         setSize(600, 400);
         setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
     }
 
 }
